4062b38911f42ac504ea03830466fa01dad286c1
Duncan Robbins and Saul Ordonez <duncan+saul@>
2019-10-22 11:27:17 -0700

    add button to revert changes to normal

--- js/scripts.js
+++ js/scripts.js
@@ -19,5 +19,17 @@ $(document).ready(function() {
     $("#initial-showing4").slideToggle();
   });
 
+  $("#font-change").click(function(){
+    $("body").addClass("font-to-serif");
+  });
 
+  $("#img-change").click(function(){
+    $("img").addClass("img-shape");
+  });
+
+  $("#change-reset").click(function(){
+    $("body").removeClass();
+    $("img").removeClass();
+    console.log("test")
+  });
 });
